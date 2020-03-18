59e890cc19b21745cb1185cd77cc28e87684e902
yujingsss <shiy173@newschool.edu>
2020-03-18 19:51:19 -0400

    modified

--- app.js
+++ app.js
@@ -7,46 +7,83 @@ let server = require("http").createServer(app);
 let io = require("socket.io")(server);
 let port = process.env.PORT || 2000;
 
+let clients = [];
+
+let adminHost = "Tyrell Corporation";
+const moment = require('moment');
+
+let generateMessage = (from, text) => {
+    return {
+        from,
+        text,
+        createdAt: moment().valueOf()
+    };
+};
+let generateLocationMessage = (from, lat, long) => {
+    return {
+        from,
+        url: `https://www.google.com/maps?q=${lat},${long}`,
+        createdAt: moment().valueOf()
+    }
+};
+
 server.listen(port, function () {
     console.log(`server listening on localhost:${port}`);
 });
 
 io.on("connection", function (client) {
-    console.log('a new user connected');
-
+    console.log('connected');
+    let _id;
     //initial stage
-    client.emit('message', {
-        from: 'admin',
-        text: 'Welcome, cyberpunker...',
-        createdAt: new Date().getTime()
-    });
+    client.emit('introgreeting', generateMessage(adminHost, 'Welcome...')
+    // {   from: 'admin',
+    //     text: 'Welcome, cyberpunker...',
+    //     createdAt: new Date().getTime()}
+    );
 
     //everyone except the user get the msg,"someone entered"
-    client.broadcast.emit('message', {
-        from: 'admin',
-        text: 'New existence joined',
-        createdAt: new Date().getTime()
+    //sending to all clients except sender
+    client.broadcast.emit('introgreeting', generateMessage('Broadcast', 'New replicant online'));
+
+    client.on('createMessage', (msg, callback) => {
+        console.log('createMessage', msg);
+        // broadcast to everyone including sender
+        io.emit('newMessage', generateMessage(msg.from, msg.text));
+        callback('this is the server:');
     });
 
-    client.on('createMessage', (msg) => {
-        console.log('createMessage', msg);
-        // broadcast to everyone
-        io.emit('message', {
-            from: msg.from,
-            text: msg.text,
-            createdAt: new Date().getTime()
-        });
+    client.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage(coords.from, coords.lat, coords.long));
+    });
 
-        // //sending to all clients except sender
-        // client.broadcast.emit('message', {
-        //     from: msg.from,
-        //     text: msg.text,
-        //     createdAt: new Date().getTime()
-        // });
+    client.on('createName', (msg) => {
+        _id = client.id;
+        console.log('createName', msg);
+        // clients.splice(0 , 0, `\n${msg.text}`);
+        clients.push(msg.text);
+        console.log(`user ${msg.text} entered`);
+        console.log('all users: ' + clients);
+        io.emit('introgreeting', generateMessage('Broadcast', `${msg.text} entered...`));
+        client.emit('newMessage', generateMessage(adminHost, `Welcome, replicant #${msg.text}`));
+        client.broadcast.emit('newMessage', generateMessage('Broadcast', `${msg.text} joined chat...`));
+        io.emit('availableusers', clients);
+        _id = msg.text;
     });
 
     client.on('disconnect', () => {
-        console.log('an user disconnected...');
+        console.log(`an user ${_id} disconnected...`);
+        client.broadcast.emit('userquit', _id);
+        console.log(`${_id} quit`);
+        client.broadcast.emit('introgreeting', generateMessage('Broadcast', 'A replicant left...'));
+        client.broadcast.emit('newMessage', generateMessage('Broadcast', `${_id} left...`));
+        for (let i = 0; i< clients.length; i++) {
+            if (_id == clients[i]) {
+                // console.log("T");
+                clients.splice(i,1);
+                // console.log(clients);
+            }
+        }
+        client.broadcast.emit('availableusers', clients);
     });
 });
 
--- public/main.js
+++ public/main.js
@@ -3,21 +3,158 @@ let socket = io();
 
 socket.on('connect', function () {
     console.log('connection established...');
-
-    // socket.emit('createMessage', {
-    //     from: 'client',
-    //     text: 'server'
-    // });
 });
-
-socket.on('message', function (msg) {
-    // console.log(`${msg.from}: ${msg.text} at ${msg.createdAt}`);
-    console.log('message', msg);
-});
-
-//socket.on - listening an event
+//socket.on - listening an disconnect event
 socket.on('disconnect', function () {
     console.log('lost connection...');
+    // alert('lost connection...');
+});
+socket.on('userquit', function (msg) {
+    console.log(msg + ' quit');
 });
 
+socket.on('introgreeting', function (msg) {
+    const formatTime = moment(msg.createdAt).format('lll');
+    // console.log(`${msg.from}: ${msg.text} at ${msg.createdAt}`);
+    // console.log('introgreeting', msg);
+    let clientname = document.createElement('li');
+    clientname.innerText = `${msg.from}, ${formatTime}:\n`;
+    clientname.style.color = "rgba(0, 255, 0, 20)";
+    clientname.style.listStyle = "none";
+    let li = document.createElement('li');
+    li.style.listStyle = "none";
+    li.innerText = `${msg.text}\n\n`;
+    document.querySelector('#typing').append(clientname, li);
+});
+socket.on('newMessage', function (msg) {
+    const formatTime = moment(msg.createdAt).format('lll');
+    // console.log('newMessage', msg);
+    let chatcontent = document.createElement('li');
+    chatcontent.innerText = `${msg.from}, ${formatTime}:\n`;
+    chatcontent.style.color = "rgba(0, 255, 0, 10)";
+    chatcontent.style.listStyle = "none";
+    chatcontent.style.fontSize="13px";
+    let li = document.createElement('li');
+    li.style.fontSize = "16px";
+    li.style.listStyle = "none";
+    li.innerText = `${msg.text}\n\n`;
+    document.querySelector('#chatbox').append(chatcontent, li);
+});
+
+//available users
+socket.on('availableusers', function (data) {
+    // console.log(data);
+    document.getElementById('allusers').innerText = data;
+});
+
+//socket.emit(argument1, argument2, callback);
+// socket.emit('createMessage', {
+//     from: 'x',
+//     text: 'hi'
+// }, function (msg) {
+//     console.log(msg, 'got it');
+// });
+
+document.querySelector('#username').addEventListener('keyup', function (event) {
+    if (event.keyCode === 13) {
+        let username = document.getElementsByName("nickname")[0].value;
+        // console.log(document.getElementsByName("nickname")); = node.list
+        let password = document.querySelector('#password');
+        if (password.value === 'replicant' && username != '') {
+            // console.log("socket.id: " + socket.id);
+            // event.preventDefault();
+            // console.log(`Welcome, ${username}`);
+            hideinputname();
+            socket.emit('createName', {
+                from: 'User',
+                text: username
+            });
+        } if (password.value != 'replicant' && username != '') {
+            let hint = document.createElement('p');
+            hint.innerText = 'please enter your correct identity, hint: Blade Runner';
+            document.querySelector('#username').appendChild(hint);
+        } if (username === '' && password.value === 'replicant') {
+            let hint = document.createElement('p');
+            hint.innerText = 'please enter a name';
+            document.querySelector('#username').appendChild(hint);
+        } if (username === '' && password.value != 'replicant') {
+            let hint = document.createElement('p');
+            hint.innerText = 'please enter a name';
+            document.querySelector('#username').appendChild(hint);
+        }
+    }
+});
+function hideinputname() {
+    document.getElementById("intropage").style.display = "none";
+    enterchatroom();
+}
+function enterchatroom() {
+    document.getElementById("chatroom").style.display = "flex";
+    let yourname = document.getElementById("yourname");
+    yourname.style.color = "rgba(0,255,0,20)";
+    yourname.innerText = document.getElementsByName("nickname")[0].value;
+}
+
+document.querySelector('#submitmessage').addEventListener('click', function (event) {
+    let messagecontent = document.getElementsByName("messagecontent")[0].value;
+    let username = document.getElementsByName("nickname")[0].value;
+    event.preventDefault();
+    socket.emit('createMessage', {
+        from: username,
+        text: messagecontent
+    }, function (msg) { console.log(msg + "send messages"); });
+    document.getElementsByName("messagecontent")[0].value = '';
+});
+
+document.querySelector('#send-location').addEventListener('click', function () {
+    event.preventDefault();
+    let username = document.getElementsByName("nickname")[0].value;
+    if (!navigator.geolocation) {
+        return alert('You are at Nowhere');
+    }
+    navigator.geolocation.getCurrentPosition(function (position) {
+        // console.log(position);
+        socket.emit('createLocationMessage', {
+            from: username,
+            lat: position.coords.latitude,
+            long: position.coords.longitude
+        });
+    }, function () {
+        alert('unable to fetch location');
+    });
+});
+
+socket.on('newLocationMessage', function (msg) {
+    // console.log('newLocationMessage', msg);
+    const formatTime = moment(msg.createdAt).format('lll');
+    let chatcontent = document.createElement('li');
+    chatcontent.innerText = `${msg.from}, ${formatTime}:\n`;
+    chatcontent.style.color = "rgba(0, 255, 0, 20)";
+    chatcontent.style.listStyle = "none";
+    let li = document.createElement('li');
+    let a = document.createElement('a');
+    a.setAttribute('target', '_blank');
+    a.setAttribute('href', msg.url);
+    a.innerText = `My Current Spot\n\n`;
+    a.style.textDecoration = "underline";
+    a.style.color = "white";
+    a.addEventListener('mouseenter', function () {
+        a.style.color = "red";
+    });
+    a.addEventListener('mouseleave', function () {
+        a.style.color = "white";
+    });
+    li.style.listStyle = "none";
+    li.appendChild(a);
+    document.querySelector('#chatbox').append(chatcontent, li);
+});
+
+socket.on('connect_error', (error) => {
+    console.log('connection error...');
+    // alert('connection error...');
+});
+socket.on('error', (error) => {
+    console.log('error occurs...');
+    // alert('error occurs...');
+});
 
